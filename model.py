4fed27c439fa69564869fc50cd68193ee770709d
JunChen <cjcj46262@gmail.com>
2025-06-02 18:54:51 +0800

    modify

--- model.py
+++ model.py
@@ -1,6 +1,4 @@
-'''
 
-'''
 import numpy as np
 import sys
 
@@ -28,16 +26,6 @@ num_bond_direction = 3
 
 
 class GINConv(MessagePassing):
-    """
-    Extension of GIN aggregation to incorporate edge information by concatenation.
-
-    Args:
-        emb_dim (int): dimensionality of embeddings for nodes and edges.
-        embed_input (bool): whether to embed input or not.
-
-
-    See https://arxiv.org/abs/1810.00826
-    """
 
     def __init__(self, emb_dim, aggr="add"):
         super(GINConv, self).__init__()
@@ -224,19 +212,6 @@ class GraphSAGEConv(MessagePassing):
 
 
 class GNN(torch.nn.Module):
-    """
-    Args:
-        num_layer (int): the number of GNN layers
-        emb_dim (int): dimensionality of embeddings
-        JK (str): last, concat, max or sum.
-        max_pool_layer (int): the layer from which we use max pool rather than add pool for neighbor aggregation
-        drop_ratio (float): dropout rate
-        gnn_type: gin, gcn, graphsage, gat
-
-    Output:
-        node representations
-
-    """
 
     def __init__(self, num_layer, emb_dim, JK="last", drop_ratio=0, gnn_type="gin", surgery=False, moe=False, rank=30, num_experts=8, index=0, order=1, topk=8):
         super(GNN, self).__init__()
@@ -388,7 +363,7 @@ class GNN(torch.nn.Module):
                     _,wd = self.TWD.got_dist(f_s=moe_dense_list[self.index], f_t=moe_dense_list[i], A=b_A, mask=mask)
                     wds.append(-wd)
                 moe_score = torch.stack(wds, dim=1)
-                wd_list.append(F.softmax(moe_score / 0.4, dim=1))   #######heatmap
+                # wd_list.append(F.softmax(moe_score / 0.4, dim=1))   #######heatmap
                 moe_score = moe_score[batch, :]
                 topk_values, topk_indices = torch.topk(moe_score, self.topk, dim=1)
                 T = 0.03
@@ -450,8 +425,6 @@ class GNN(torch.nn.Module):
         return node_representation
 
     def _initialize_weights(self):
-    # https://github.com/facebookresearch/deepcluster/blob/main/models/alexnet.py
-    # nn.Embedding is initialized by xavier_uniform_ from the original gnn code    
         for y, m in enumerate(self.modules()):
             if isinstance(m, nn.BatchNorm1d):
                 m.weight.data.fill_(1)
@@ -621,154 +594,6 @@ class expert(torch.nn.Module):
 
 
 
-
-
-
-
-class GNN_topexpert(torch.nn.Module): # expert 를 parallel 하게
-
-    def __init__(self, args, criterion):
-        super(GNN_topexpert, self).__init__()
-        self.num_layer = args.num_layer
-        self.drop_ratio = args.dropout_ratio
-        self.JK = args.JK
-        self.emb_dim = args.emb_dim
-        self.num_tasks = args.num_tasks
-        self.graph_pooling = args.graph_pooling
-        self.gnn_type = args.gnn_type
-
-        self.gate = gate(args.emb_dim, args.gate_dim)
-        self.cluster = nn.Parameter(torch.Tensor(args.num_experts, args.gate_dim))
-        torch.nn.init.xavier_normal_(self.cluster.data)
-        ## optimal transport
-        self.scf_emb = nn.Parameter(torch.Tensor(args.num_tr_scf, args.gate_dim))
-        torch.nn.init.xavier_normal_(self.scf_emb.data)
-        self.cos_similarity = nn.CosineSimilarity(dim=1, eps=1e-6)
-
-    
-        self.T = 10 
-        self.criterion = criterion
-        self.num_experts = args.num_experts
-
-        self.experts_w = nn.Parameter(torch.empty(self.emb_dim, self.num_tasks * self.num_experts))
-        self.experts_b =  nn.Parameter(torch.empty(self.num_tasks * self.num_experts))
-        self.reset_experts()
-
-        self.gate_pool = global_add_pool
-        
-    
-        if self.num_layer < 2:
-            raise ValueError("Number of GNN layers must be greater than 1.")
-
-        self.gnn = GNN(self.num_layer, self.emb_dim, self.JK, self.drop_ratio, gnn_type=self.gnn_type)
-        
-        # Different kind of graph pooling
-        if self.graph_pooling == "sum":
-            self.pool = global_add_pool
-        elif self.graph_pooling == "mean":
-            self.pool = global_mean_pool
-        elif self.graph_pooling == "max":
-            self.pool = global_max_pool
-        elif self.graph_pooling == "attention":
-            if self.JK == "concat":
-                self.pool = GlobalAttention(gate_nn=torch.nn.Linear((self.num_layer + 1) * self.emb_dim, 1))
-            else:
-                self.pool = GlobalAttention(gate_nn=torch.nn.Linear(self.emb_dim, 1))
-        elif self.graph_pooling[:-1] == "set2set":
-            set2set_iter = int(self.graph_pooling[-1])
-            if self.JK == "concat":
-                self.pool = Set2Set((self.num_layer + 1) * self.emb_dim, set2set_iter)
-            else:
-                self.pool = Set2Set(self.emb_dim, set2set_iter)
-        else:
-            raise ValueError("Invalid graph pooling type.")
-
-
-    def reset_experts(self):
-        torch.nn.init.kaiming_uniform_(self.experts_w, a=math.sqrt(5))
-        bound = 1 / math.sqrt(self.emb_dim) 
-        torch.nn.init.uniform_(self.experts_b, -bound, bound)
-        
-
-    def from_pretrained(self, model_file):
-        self.gnn.load_state_dict(torch.load(model_file), strict=False)
-
-
-    def forward(self, data):
-
-        x, edge_index, edge_attr, batch = data.x, data.edge_index, data.edge_attr, data.batch
-
-        node_rep = self.gnn(x, edge_index, edge_attr)
-        gnn_out = self.pool(node_rep, batch)
-        gate_input = self.gate_pool(node_rep, batch)
-
-        ## multi-head mlps
-        gnn_out = torch.unsqueeze(gnn_out, -1) # N x emb_dim x 1
-        gnn_out = gnn_out.repeat(1, 1, self.num_tasks * self.num_experts) # N x emb_dim x (tasks * experts)
-
-        clf_logit = torch.sum(gnn_out * self.experts_w, dim=1) + self.experts_b # N x (tasks * experts)
-        
-        clf_logit = clf_logit.view(-1, self.num_tasks, self.num_experts) # N  x tasks x num_experts
-        
-        
-        z = self.gate(gate_input)
-        q = self.get_q(z)
-
-        return clf_logit, z, q # N x 1 x heads
-
-
-    def assign_head(self, q):  
-        
-        q_idx = torch.argmax(q, dim=-1)  # N x 1     
-        if self.training:       
-            g = F.gumbel_softmax((q + 1e-10).log(), tau=10, hard=False, dim=-1)       
-            g = torch.unsqueeze(g, 1)
-            g = g.repeat(1, self.num_tasks, 1) # N x tasks x heads
-            return g,  q_idx # N x tasks x heads // N // N 
-        else:
-            q = torch.unsqueeze(q, 1)
-            q = q.repeat(1, self.num_tasks, 1) # N x tasks x heads
-            return q, q_idx # N x tasks x heads // N // N 
-
-    def clf_loss(self, clf_outs, labels, assign): 
-
-        is_valid = labels ** 2 > 0
-        
-        is_valid_tensor = torch.unsqueeze(is_valid,-1)
-        is_valid_tensor = is_valid_tensor.repeat(1, 1, self.num_experts)
-
-        ## calculate loss
-        labels = torch.unsqueeze(labels, -1)
-        labels = labels.repeat(1, 1, self.num_experts)  # N x tasks x heads
-        loss_tensor = self.criterion(clf_outs, (labels + 1)/2)
-
-        #### modify loss --> assign 0 to label == 0    
-        loss_tensor_valid =  torch.where(is_valid_tensor, loss_tensor, torch.zeros(loss_tensor.shape).to(loss_tensor.device).to(loss_tensor.dtype))
-
-        #### modify loss based on assign index 
-        loss_mat = torch.sum(assign * loss_tensor_valid, dim=0) # 
-        num_valid_mat = torch.sum(assign * is_valid_tensor.long(), dim=0) # tasks x head
-        
-        return loss_mat, (num_valid_mat + 1e-10) # tasks x heads
-
-    def get_q(self, z):
-
-        q = 1.0 / (1.0 + torch.sum(torch.pow(z.unsqueeze(1) - self.cluster, 2), 2) )
-        q = q.pow((1.0 + 1.0) / 2.0)
-        q = (q.t() / torch.sum(q, 1)).t()
-        return q
-
-    def target_distribution(self, q):
-        weight = q**2 / q.sum(0)
-        p = (weight.t() / weight.sum(1)).t()
-        return p
-
-    def alignment_loss(self, scf_idx, q):
-        e = self.scf_emb[scf_idx] 
-        e = e.unsqueeze(dim=-1)
-        mu = torch.transpose(self.cluster, 1, 0).unsqueeze(dim=0)
-        loss = torch.mean(torch.sum(q * (1 - self.cos_similarity(e, mu)), dim=1))
-        return loss
     
 
 if __name__ == "__main__":
